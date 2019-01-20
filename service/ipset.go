f6d70969ffaaf880a455d66d266b222c4d3f9b3c
Hamed Bahadorzadeh <bahadorzadeh@sindad.com>
2019-01-20 12:29:18 +0330

    iptables related work removed

---
+++ service/ipset.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	"github.com/hbahadorzadeh/wit/model"
+	"github.com/janeczku/go-ipset/ipset"
+	"log"
+	"sync"
+)
+
+type IpsetService struct {
+	once  sync.Once
+	value *ipset.IPSet
+}
+
+func (s *IpsetService) GetInstance(config model.Config, ipts IpTables) *ipset.IPSet {
+	s.once.Do(func() {
+		s.value = s.getIpsetService(config, ipts)
+	})
+	return s.value
+}
+
+func (is *IpsetService) getIpsetService(config model.Config, ipts IpTables) *ipset.IPSet {
+	res, err := ipset.New(config.ListName, "hash:ip", &ipset.Params{})
+	if err != nil {
+		log.Panic(err)
+	}
+	ipts.Init(config)
+	return res
+}
